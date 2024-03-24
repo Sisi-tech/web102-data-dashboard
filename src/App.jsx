cf103accdbd3a4f1ec4d196c805541379e29a07b
Sisi Wang <sisiwang242@gmail.com>
2024-03-24 16:49:15 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { ssrImportKey } from 'vite/runtime'
+import './App.css'
+import axios from 'axios';
+
+function App() {
+  useEffect(() => {
+
+  }, []);
+
+  return (
+    <div className="bg-[url('./assets/sky.jpg')] bg-cover min-h-screen min-w-full">
+    </div>
+  )
+}
+
+export default App
